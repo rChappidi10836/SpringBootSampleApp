0decf89b22a16e7938050af441307045d76a76e0
Chappidi <rchappidi@teksystems.com>
2023-05-06 00:15:03 +0530

    for sample aws app

---
+++ SpringBootSampleApp/src/main/java/com/example/SpringBootSampleApp/SpringBootSample.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSampleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSample {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSample.class, args);
+	}
+
+}
---
+++ SpringBootSampleApp/src/test/java/com/example/SpringBootSampleApp/SpringBootSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSampleApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
